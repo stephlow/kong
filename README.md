b6a4898d0c93b2f4227ed0cbc129813f460d9459
Stephan Lagerwaard <stephanlagerwaard@gmail.com>
2017-05-22 17:35:04 +0200

    Some tweaks on the docs

--- README.md
+++ README.md
@@ -4,11 +4,11 @@ Simple wrapper around the Kong Admin REST API.
 
 ## Requirements
 
-- Kong
+- Kong (0.10.x)
 
 ## Installation
 
-Kong is [available in Hex](https://hex.pm/docs/publish), the package can be installed
+Kong is [available in Hex](https://hex.pm/packages/kong), the package can be installed
 by adding `kong` to your list of dependencies in `mix.exs`:
 
 ```elixir
@@ -24,6 +24,31 @@ You can configure which Node the client connects with. If not set, it will defau
 ```elixir
 config :kong, host: "http://example.com:8001"
 ```
+
+## Examples
+
+This library mostly maps Elixir types to Kong Admin API requests and responses. You should consult the Kong documentation on how to form your requests.
+
+List APIs:
+
+```elixir
+{:ok, apis} = Kong.API.list
+```
+
+Passing Querystring parameters to the list call:
+
+```elixir
+{:ok, page_one} = Kong.API.list size: 10
+{:ok, page_two} = Kong.API.list size: 10, offset: page_one["offset"]
+```
+
+Creating a new API:
+
+```elixir
+{:ok, api} = Kong.API.add %{name: "Testing", upstream_url: "http://localhost:4000/api", hosts: ["api.example.com"]}
+```
+
+## Documentation
 
 Documentation can be found at [https://hexdocs.pm/kong](https://hexdocs.pm/kong).
 
--- lib/api.ex
+++ lib/api.ex
@@ -1,35 +1,55 @@
 defmodule Kong.API do
+  @moduledoc """
+  Provides access to the Kong endpoints for API management.
+
+  Consult the Kong Admin API documentation for more information about the API object properties
+  """
+
   @endpoint "/apis"
 
   import Kong, only: [get: 1, get: 3, post: 2, patch: 2, put: 2, delete: 1]
 
   @doc """
-  Add API
+  Adds a new API to Kong with the given map `attributes`.
+
+  Returns either `{:ok, result}` where result is a map with the newly created API or `{:error, reasons}` where reasons is a map containing information.
   """
   def add(attributes), do: post(@endpoint, attributes)
 
   @doc """
-  Retrieve API
+  Retrieve an API from Kong with the given `name_or_id` string.
+
+  Returns either `{:ok, result}` where result is a map with the API or `{:error, reasons}` where reasons is a map containing information.
   """
   def retrieve(name_or_id), do: get(@endpoint <> "/#{name_or_id}")
 
   @doc """
-  List APIs
+  List APIs from Kong based on the given `opts` Keyword List.
+
+  Keywords defined in opts are mapped to the Querystring parameters of the request to Kong.
+
+  Returns either `{:ok, result}` where result is the paginated result returned by Kong, or `{:error, reasons}` where reasons is a map containing information.
   """
   def list(opts \\ []), do: get(@endpoint, [], params: opts)
 
   @doc """
-  Update API
+  Update API in Kong with the given string `name_or_id` with the given map `attributes`.
+
+  Returns either `{:ok, result}` where result is a map with the updated API or `{:error, reasons}` where reasons is a map containing information.
   """
   def update(name_or_id, attributes), do: patch(@endpoint <> "/#{name_or_id}", attributes)
 
   @doc """
-  Update Or Create API
+  Update Or Create API in Kong with the given map `attributes`.
+
+  Returns either `{:ok, result}` where result is a map with the created / updated API or `{:error, reasons}` where reasons is a map containing information.
   """
   def update_or_create(attributes), do: put(@endpoint, attributes)
 
   @doc """
-  Remove API
+  Remove API from Kong with the given string `name_or_id`.
+
+  Returns either `{:ok, result}` where result is a map with the newly created API or `{:error, reasons}` where reasons is a map containing information.
   """
   def remove(name_or_id), do: delete(@endpoint <> "/#{name_or_id}")
 
